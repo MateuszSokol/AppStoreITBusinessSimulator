0579d57c4748e74565048b7fbcabbbfa0a155a67
mateusz1231 <matizek2@vp.pl>
2022-02-08 21:40:13 +0100

    Added Boss object

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import com.company.mateuszsCompany.Boss;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Hello, welcome to ITSimulator"+'\n'+"The aim of the game is to make 3 elaborateProjects " +
+                "without using boss. At least 1 projects must be earned by hire seller and u must have more cash than in the start");
+        Scanner scanner = new Scanner(System.in);
+        Boss me = new Boss("Matt","Sprout");
+        //generate starting resources
+        me.generateRandomCashAmount();
+        me.insertAbilitiesToBoss(me);
+
+        
+        System.out.println(me.getCash());
     }
 }
--- src/com/company/mateuszsCompany/Boss.java
+++ src/com/company/mateuszsCompany/Boss.java
@@ -2,17 +2,18 @@ package com.company.mateuszsCompany;
 
 import com.company.Abilities;
 
+import java.util.Random;
+
 public class Boss {
     String bossName;
     String bossLastName;
     Double cash;
-    Abilities [] bossAbilities;
+    public Abilities [] bossAbilities;
 
 
-    public Boss(String bossName, String bossLastName, Double cash) {
+    public Boss(String bossName, String bossLastName) {
         this.bossName = bossName;
         this.bossLastName = bossLastName;
-        this.cash = cash;
         this.bossAbilities = new Abilities[5];
     }
 
@@ -22,5 +23,20 @@ public class Boss {
         boss.bossAbilities[2] = Abilities.WORDPRESS;
         boss.bossAbilities[3] = Abilities.PRESTASHOP;
         boss.bossAbilities[4] = Abilities.DATA_BASE;
+    }
+
+
+    public void generateRandomCashAmount(){
+        System.out.println("Generating random cash amount");
+        Random random = new Random();
+        this.cash = (double) Math.round(random.nextDouble(10, 100));
+    }
+
+    public Double getCash() {
+        return cash;
+    }
+
+    public void setCash(Double cash) {
+        this.cash = cash;
     }
 }
